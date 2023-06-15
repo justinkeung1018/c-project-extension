a5e0f61e781e62f42fe4290c730ba67634121dfa
Sonny Ku <sk2722@ic.ac.uk>
2023-06-15 15:07:15 +0100

    Added simple commands to be edited

---
+++ hints.c
@@ -0,0 +1,15 @@
+#include "raylib.h"
+
+
+// DrawText("Controls:", 20, 20, 10, BLACK);
+// DrawText("- Right/Left to move", 40, 40, 10, DARKGRAY);
+// DrawText("- Space to jump", 40, 60, 10, DARKGRAY);
+// DrawText("- Mouse Wheel to Zoom in-out, R to reset zoom", 40, 80, 10, DARKGRAY);
+// DrawText("- C to change camera mode", 40, 100, 10, DARKGRAY);
+
+
+/*
+Up Arrow - accelerate
+Left Arrow - steer left
+Right Arrow - steer right
+*/

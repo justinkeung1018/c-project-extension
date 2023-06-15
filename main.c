c5a2f4f3262e19a9f4cd72d1636d6cca2db23ce9
Sonny Ku <sk2722@ic.ac.uk>
2023-06-15 21:52:22 +0100

    Completely removed magic numbers

--- main.c
+++ main.c
@@ -15,6 +15,17 @@
 
 // Padding 
 #define SMALL_PADDING 10
+#define MEDIUM_PADDING 50
+#define LARGE_PADDING 100
+#define EXTRA_LARGE_PADDING 1000
+#define FPS_PADDING   80
+
+// Various screen sizes
+#define SCREEN_WIDTH GetScreenWidth()
+#define SCREEN_HEIGHT GetScreenHeight()
+#define HALF_SCREEN_WIDTH_SIZE GetScreenWidth() / 2
+#define HALF_SCREEN_HEIGHT_SIZE GetScreenHeight() / 2
+#define QUARTER_SCREEN_HEIGHT_SIZE GetScreenHeight() / 4
 
 static void display_exit_screen(void) {
   const char exit_message[] = "Are you sure you want to quit? [Y/N]";
@@ -23,18 +34,19 @@ static void display_exit_screen(void) {
   int exit_string_width = MeasureText(exit_message, LARGE_FONT_SIZE);
   int save_string_width = MeasureText(save_message, MEDIUM_FONT_SIZE);
 
-  DrawRectangle(0, GetScreenHeight() / 4 + 50, GetScreenWidth(), 1000, Fade(BLUE, 0.5f));
-  DrawRectangleLines(0, GetScreenHeight() / 4 + 50, GetScreenWidth(), 1000, RED);
-  DrawText(exit_message, GetScreenWidth() / 2 - exit_string_width / 2, GetScreenHeight() / 2 - 130, LARGE_FONT_SIZE, WHITE);
-  DrawText(save_message, GetScreenWidth() / 2 - save_string_width / 2, GetScreenHeight() / 2 + 10, MEDIUM_FONT_SIZE, WHITE);
+  DrawRectangle(0, QUARTER_SCREEN_HEIGHT_SIZE + MEDIUM_PADDING, SCREEN_WIDTH, EXTRA_LARGE_PADDING, Fade(BLUE, 0.5f));
+  DrawRectangleLines(0, QUARTER_SCREEN_HEIGHT_SIZE + MEDIUM_PADDING, SCREEN_WIDTH, EXTRA_LARGE_PADDING, RED);
+  DrawText(exit_message, HALF_SCREEN_WIDTH_SIZE - exit_string_width / 2, HALF_SCREEN_HEIGHT_SIZE - LARGE_PADDING, LARGE_FONT_SIZE, WHITE);
+  DrawText(save_message, HALF_SCREEN_WIDTH_SIZE - save_string_width / 2, HALF_SCREEN_HEIGHT_SIZE + SMALL_PADDING, MEDIUM_FONT_SIZE, WHITE);
 }
 
 static void display_debugging_stats(void) {
-  DrawFPS(GetScreenWidth() - 80, SMALL_PADDING + SMALL_TEXT_HEIGHT);
-  const char *resolution_text = TextFormat("Screen Resolution: %d x %d", GetScreenWidth(), GetScreenHeight());
+  const char *resolution_text = TextFormat("Screen Resolution: %d x %d", SCREEN_WIDTH, SCREEN_HEIGHT);
 
   int screen_res_string_width = MeasureText(resolution_text, EXTRA_SMALL_FONT_SIZE);
+
   DrawText(resolution_text, GetScreenWidth() - SMALL_PADDING - screen_res_string_width, SMALL_PADDING, EXTRA_SMALL_FONT_SIZE, LIME);
+  DrawFPS(GetScreenWidth() - FPS_PADDING, SMALL_PADDING + SMALL_TEXT_HEIGHT);
 }
 
 int main(void) {
@@ -71,17 +83,13 @@ int main(void) {
 
     if (exit_window_requested) {
       // save data here
+      display_exit_screen();
       if (IsKeyPressed(KEY_Y) || IsKeyPressed(KEY_ENTER)) {
         exit_window = true;
       } else if (IsKeyPressed(KEY_N)) {
         exit_window_requested = false;
       }
     }
-
-    if (exit_window_requested) {
-      display_exit_screen();
-    }
-
 
     UpdateMusicStream(music);
 
@@ -103,7 +111,7 @@ int main(void) {
       spaceship_rotate_right(spaceship);
     }
 
-    DrawText("Press F1 for Debugging Stats", 10, GetScreenHeight() - 40, SMALL_FONT_SIZE, WHITE);
+    DrawText("Press F1 for Debugging Stats", SMALL_PADDING, SCREEN_HEIGHT - MEDIUM_PADDING, SMALL_FONT_SIZE, WHITE);
     if (IsKeyDown(KEY_F1)) {
       display_debugging_stats();
     }

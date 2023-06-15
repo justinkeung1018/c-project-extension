fc1d5cdf7cffb460108be7910cec0e51e348aadb
Sonny Ku <sk2722@ic.ac.uk>
2023-06-15 16:26:26 +0100

    Started writing static function of control display in main.c

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 
 #define FPS 60
 
+static void display_controls(void) {
+  // draw text
+}
+
 int main(void) {
   // [Initialise Screen]
   InitWindow(0, 0, "Asteroids");
@@ -26,14 +30,18 @@ int main(void) {
       PlaySound(sound); // combine this with other components
     }
 
+    if (IsKeyPressed(KEY_TAB)) {
+      // display controls
+    }
+
     BeginDrawing();
 
-      ClearBackground(BLACK);
-      DrawText("Press F1 for Debugging Stats", 10, screen_height - 20, 20, WHITE);
-      if (IsKeyDown(KEY_F1)) {
-        DrawFPS(screen_width - 90, 10);
-        DrawText(TextFormat("Screen Resolution: %d x %d", screen_width, screen_height), 10, 10, 20, LIME);
-      }
+    ClearBackground(BLACK);
+    DrawText("Press F1 for Debugging Stats", 10, screen_height - 20, 20, WHITE);
+    if (IsKeyDown(KEY_F1)) {
+      DrawFPS(screen_width - 90, 10);
+      DrawText(TextFormat("Screen Resolution: %d x %d", screen_width, screen_height), 10, 10, 20, LIME);
+    }
 
     EndDrawing();
   }

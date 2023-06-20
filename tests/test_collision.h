9fc7474d4379053bcdb1f043085790e6f5328e8d
justinkeung <justinkeung1018@gmail.com>
2023-06-20 21:17:59 +0100

    Add doxygen

--- tests/test_collision.h
+++ tests/test_collision.h
@@ -1,7 +1,14 @@
 #ifndef TEST_COLLISION_H
 #define TEST_COLLISION_H
 
+/**
+ * Tests for the collides_asteroid_spaceship function.
+ */
 extern void test_collides_asteroid_spaceship(void);
+
+/**
+ * Tests for the collides_asteroid_bullet function.
+ */
 extern void test_collides_asteroid_bullet(void);
 
 #endif

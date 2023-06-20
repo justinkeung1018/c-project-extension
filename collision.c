602cfea33e9a0a75863f737d3c1e57bb82ab43c1
justinkeung <justinkeung1018@gmail.com>
2023-06-20 15:46:44 +0100

    Check if asteroid is entirely within or surrounded by spaceship

--- collision.c
+++ collision.c
@@ -9,6 +9,33 @@ static Vector2 project(Vector2 vector, Vector2 onto) {
 
 bool collides_asteroid_spaceship(Asteroid a, Spaceship s) {
   int length = s->collider.length;
+
+  // Check if any vertex of the spaceship is inside the asteroid
+  for (int i = 0; i < length; i++) {
+    Vector2 vector = s->collider.vectors[i];
+    if (Vector2DistanceSqr(vector, a->position) <= a->size * a->size) {
+      return true;
+    }
+  }
+
+  // Check if the centre of the asteroid is inside the spaceship
+  bool centre_in_spaceship = true;
+  for (int i = 0; i < length; i++) {
+    Vector2 v1 = s->collider.vectors[i];
+    Vector2 v2 = s->collider.vectors[(i + 1) % length];
+    Vector2 edge = Vector2Subtract(v1, v2);
+    Vector2 normal = Vector2Rotate(edge, -M_PI / 2);
+
+    Vector2 v1_to_centre = Vector2Subtract(a->position, v1);
+
+    float dot_product = Vector2DotProduct(normal, v1_to_centre);
+    centre_in_spaceship = centre_in_spaceship && (dot_product > 0);
+  }
+
+  if (centre_in_spaceship) {
+    return true;
+  }
+
   for (int i = 0; i < length; i++) {
     Vector2 v1 = s->collider.vectors[i];
     Vector2 v2 = s->collider.vectors[(i + 1) % length];

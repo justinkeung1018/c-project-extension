4fc9968a9f4afce917baab273496849c8a2d8992
Sonny Ku <sk2722@ic.ac.uk>
2023-06-20 23:23:02 +0100

    Added deoxygen comments to loading.h

--- loading.h
+++ loading.h
@@ -24,12 +24,32 @@ struct Loader {
 
 typedef struct Loader *Loader;
 
+/**
+ * Initialises the loader variable.
+ *
+ * @return The loader.
+ */
 extern Loader loading_initialise(void);
 
+/**
+ * Updates the variables of the loader to allow the animation to occur.
+ *
+ * @param The loader.
+ */
 extern void update_variables(Loader loader);
 
+/**
+ * Draws all the animations of the loading screen.
+ *
+ * @param The loader.
+ */
 extern void display_loading_animation(Loader loader);
 
+/**
+ * Frees the loader variable.
+ *
+ * @param The loader.
+ */
 extern void loader_free(Loader loader);
 
 #endif

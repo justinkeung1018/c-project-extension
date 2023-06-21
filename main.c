b4931220bdd7b669b5f044af9bdfe23573eef510
Sonny Ku <sk2722@ic.ac.uk>
2023-06-21 22:21:04 +0100

    Added game mode selection screen

--- main.c
+++ main.c
@@ -18,6 +18,7 @@
 #define SMALL_FONT_SIZE            40
 #define MEDIUM_FONT_SIZE           80
 #define LARGE_FONT_SIZE            100
+#define EXTRA_LARGE_FONT_SIZE      200
 
 // Text height
 #define SMALL_TEXT_HEIGHT          20
@@ -35,6 +36,57 @@
 #define HALF_SCREEN_WIDTH_SIZE     GetScreenWidth() / 2
 #define HALF_SCREEN_HEIGHT_SIZE    GetScreenHeight() / 2
 #define QUARTER_SCREEN_HEIGHT_SIZE GetScreenHeight() / 4
+
+static void display_and_select_gamemode(int selection) {
+  int select_gamemode_string_width = MeasureText("Select Gamemode", EXTRA_LARGE_FONT_SIZE);
+  int singleplayer_string_width = MeasureText("Singleplayer", LARGE_FONT_SIZE);
+  int multiplayer_string_width = MeasureText("Multiplayer", LARGE_FONT_SIZE);
+  int confirmation_string_width = MeasureText("Press ENTER to Confirm", SMALL_FONT_SIZE);
+
+  int singleplayer_string_x = HALF_SCREEN_WIDTH_SIZE - singleplayer_string_width - LARGE_PADDING;
+  int multiplayer_string_x = HALF_SCREEN_WIDTH_SIZE + LARGE_PADDING;
+  int selection_string_y = HALF_SCREEN_HEIGHT_SIZE;
+
+  DrawText(
+      "Select Gamemode",
+      HALF_SCREEN_WIDTH_SIZE - select_gamemode_string_width / 2,
+      HALF_SCREEN_HEIGHT_SIZE - LARGE_PADDING * 5,
+      EXTRA_LARGE_FONT_SIZE,
+      WHITE
+    );
+
+  DrawText(
+      "Singleplayer",
+      singleplayer_string_x,
+      selection_string_y,
+      LARGE_FONT_SIZE,
+      WHITE
+    );
+
+  DrawText(
+      "Multiplayer",
+      multiplayer_string_x,
+      selection_string_y,
+      LARGE_FONT_SIZE,
+      WHITE
+    );
+
+  DrawRectangle(
+      (selection == 0 ? singleplayer_string_x : multiplayer_string_x),
+      selection_string_y + LARGE_PADDING + SMALL_PADDING * 2,
+      (selection == 0 ? singleplayer_string_width : multiplayer_string_width),
+      10,
+      WHITE
+    );
+
+  DrawText(
+      "Press ENTER to Confirm",
+      HALF_SCREEN_WIDTH_SIZE - confirmation_string_width / 2,
+      HALF_SCREEN_HEIGHT_SIZE + QUARTER_SCREEN_HEIGHT_SIZE / 2,
+      SMALL_FONT_SIZE,
+      PURPLE
+    );
+}
 
 static void display_exit_screen(void) {
   const char exit_message[] = "Are you sure you want to quit? [Y/N]";
@@ -135,12 +187,39 @@ int main(void) {
   bool exit_window_drawn = false;
   bool exit_window = false;
 
+  /*
   while (!loader->fully_loaded) {
     BeginDrawing();
     ClearBackground(BLACK);
 
     update_variables(loader);
     display_loading_animation(loader);
+    EndDrawing();
+  }
+  */
+
+
+
+  bool selected = false;
+  int selection = 0;
+
+  while (!selected) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    if (IsKeyPressed(KEY_LEFT)) {
+      selection = 0;
+    }
+
+    if (IsKeyPressed(KEY_RIGHT)) {
+      selection = 1;
+    }
+
+    if (IsKeyPressed(KEY_ENTER)) {
+      selected = true;
+    }
+
+    display_and_select_gamemode(selection);
     EndDrawing();
   }
 

6394514f757b946deea1768392e84cbb010b3252
hn622 <hn622@ic.ac.uk>
2023-06-21 17:03:46 +0100

    fixed assert_eq_double and assert_eq_Vector2

--- tests/test_utils.c
+++ tests/test_utils.c
@@ -7,6 +7,7 @@
 #include "raylib.h"
 
 #define WIDTH 60
+#define EPSILON 0.0000001
 
 static char *boolstr(bool expr) {
   return expr ? "true" : "false";
@@ -37,7 +38,7 @@ void assert_eq_int(int expected, int result, const char *testname) {
 }
 
 void assert_eq_double(double expected, double result, const char *testname) {
-  if (fabs(result - expected) >= 0.0000001) {
+  if (fabs(result - expected) >= EPSILON) {
     printf("%-*s: expected %lf, actual %lf\n", WIDTH, testname, expected, result);
   } else {
     printf("%-*s: OK\n", WIDTH, testname);
@@ -45,7 +46,7 @@ void assert_eq_double(double expected, double result, const char *testname) {
 }
 
 void assert_eq_Vector2(Vector2 expected, Vector2 result, const char *testname) {
-  if (expected.x != result.x && expected.y != result.y) {
+  if (fabs(result.x - expected.x) >= EPSILON && fabs(result.y - expected.y) >= EPSILON) {
     printf("%-*s: expected x = %lf and y = %lf, got x = %lf and y = %lf.\n", WIDTH, testname, expected.x, expected.y, result.x, result.y);
   } else {
     printf("%-*s: OK\n", WIDTH, testname);
